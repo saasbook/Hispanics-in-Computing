803c202ec8113db91180a9f2f91017047814df0f
Amy Shen <amyshen@berkeley.edu>
2019-10-30 15:15:24 -0700

    add members placeholder page

--- app/controllers/hispanicsincomputing_controller.rb
+++ app/controllers/hispanicsincomputing_controller.rb
@@ -2,16 +2,23 @@ class HispanicsincomputingController < ApplicationController
   def index
   end
 
+  def members
+    render :layout => false
+  end 
+
   def tapia_2018
-        render :layout => false
+    render :layout => false
   end
+
   def tapia_2017
-        render :layout => false
+    render :layout => false
   end
+
   def tapia_2016
-        render :layout => false
+    render :layout => false
   end
+
   def tapia_2013
-        render :layout => false
+    render :layout => false
   end
 end
