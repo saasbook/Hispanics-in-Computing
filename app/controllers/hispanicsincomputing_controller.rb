73866aa1bd6b32219646378fd0fb6dfbdd204bf2
Amy Shen <amyshen@berkeley.edu>
2019-10-30 16:08:09 -0700

    fix layout/application

--- app/controllers/hispanicsincomputing_controller.rb
+++ app/controllers/hispanicsincomputing_controller.rb
@@ -3,22 +3,17 @@ class HispanicsincomputingController < ApplicationController
   end
 
   def members
-    render :layout => false
   end 
 
   def tapia_2018
-    render :layout => false
   end
 
   def tapia_2017
-    render :layout => false
   end
 
   def tapia_2016
-    render :layout => false
   end
 
   def tapia_2013
-    render :layout => false
   end
 end
