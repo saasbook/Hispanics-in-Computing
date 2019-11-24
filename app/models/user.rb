a646d1786fb18ac85e4e34505cb2c1b20a2451f8
Danelle Nachum <dnachum@berkeley.edu>
2019-11-24 00:32:18 -0800

    modified user.rb and migrations to merge google login with current User table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,10 @@
 class User < ActiveRecord::Base
     def self.update_or_create(auth)
-        user = User.find_by(google_uid: auth[:uid]) || User.new
+        user = User.find_by(email: auth[:info][:email]) || User.new
         user.attributes = {
-          google_uid: auth[:uid],
           first_name: auth[:info][:first_name],
           last_name: auth[:info][:last_name],
-          email: auth[:info][:email],
-          google_token: auth[:credentials][:token],
-          google_refresh_token: auth[:credentials][:refresh_token],
-          google_oauth_expires_at: auth[:credentials][:expires_at]
+          email: auth[:info][:email]
         }
         user.save!
         return user
