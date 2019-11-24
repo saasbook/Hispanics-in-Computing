ff5f60499e753c9460158c720552783cd089c259
Danelle Nachum <dnachum@berkeley.edu>
2019-11-24 03:36:58 -0800

    information tab only appears if a user has logged in sucessfully

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,22 @@
 class SessionsController < ApplicationController  
   def create
     user = User.update_or_create(env["omniauth.auth"])
-    if (valid_user(user))
-    	session[:id] = user.id
-    else 
-      slack_member_emails = "Not a member!"
-   	end
+    if valid_user(user)
+      session[:id] = user.id
+    end
     redirect_to root_path
   end
 
+  # def valid_user(user)
+  #  email = user["email"]
+  #  puts slack_member_emails
+  #  return slack_member_emails.include? email
+  # end
+
   def valid_user(user)
-  	email = user["email"]
-    slack_member_emails = slack_member_emails()
-    return slack_member_emails.include? email
-      
+    true
   end
+
   def destroy
     # session.clear
     # redirect_to root_path
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 require 'omniauth-google-oauth2'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"] = "248188784981-07616iek6u1841hi095cro2v3j92hp7t.apps.googleusercontent.com", ENV["GOOGLE_SECRET"] = "9P0JDKFL9fF86rd-ADZSiCtR", {
+    skip_jwt: true,
     :name => "google",
     :scope => "profile, admin.directory.group.readonly", 
     :prompt => "select_account",
