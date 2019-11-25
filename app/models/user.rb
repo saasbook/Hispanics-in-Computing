3681d396c749c11a0af8e36125573cc8677bdce9
Danelle Nachum <dnachum@berkeley.edu>
2019-11-25 05:23:23 -0800

    added rspec tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,19 @@ class User < ActiveRecord::Base
     return user
   end
 
-  def self.valid_user(user)
-    email = user["email"]
+  def self.valid_user(email)
     return slack_member_emails.include? email
   end
+
+  def self.slack_member_emails
+		client = Slack::Web::Client.new
+		all_members = []
+
+		client.users_list(presence: true, limit: 10, sleep_interval: 5, max_retries: 20) do |response|
+			all_members.concat(response.members.select { |member| !member["deleted"]})
+		end
+
+		return all_members.map { |member| member["email"]}
+	end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  describe ".valid_user" do
+    before(:all) do
+      @email = "bob@gmail.com"
+    end
+    context "given a user who is in the slack group" do
+      it "returns true" do
+        expect(User).to receive(:slack_member_emails).and_return([@email])
+        expect(valid_user(@email)).to be true
+      end
+    end
+    context "given a user who is not in the slack group" do
+      it "returns false" do
+        expect(User).to recieve(:slack_member_emails).and_return(["alice@gmail.com"])
+        expect(valid_user(@email)).to be false
+      end
+    end
+  end
 end
