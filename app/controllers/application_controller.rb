0d6d04c53a13d6e6aba5d843f1671dd6f6e28290
Danelle Nachum <dnachum@berkeley.edu>
2019-11-25 05:23:23 -0800

    added rspec tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,16 +12,6 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:id]) if session[:id]
-  end
-
-	def slack_member_emails
-		client = Slack::Web::Client.new
-		all_members = []
-
-		client.users_list(presence: true, limit: 10, sleep_interval: 5, max_retries: 20) do |response|
-			all_members.concat(response.members.select { |member| !member["deleted"]})
-		end
-
-		return all_members.map { |member| member["email"]}
 	end
+	
 end
