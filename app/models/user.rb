690d867ba39e87d4e60e6f527d8770d9974a4e61
Danelle Nachum <dnachum@berkeley.edu>
2019-11-25 14:32:46 -0800

    added rspec tests for validate_and_create in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
-  def self.update_or_create(auth)
-    user = User.find_by(email: auth[:info][:email]) || User.new
-    user.attributes = {
-      first_name: auth[:info][:first_name],
-      last_name: auth[:info][:last_name],
-      email: auth[:info][:email]
-    }
-    user.save!
-    return user
+  def self.validate_and_create(auth)
+    email = auth[:info][:email]
+    if User.valid_user(email)
+      user = User.find_by(:email => email) || User.new(:first_name => auth[:info][:first_name],
+                      :last_name => auth[:info][:last_name],
+                      :email => auth[:info][:email])
+      return user.save ? user : nil
+    else
+      return nil
+    end
   end
 
   def self.valid_user(email)
