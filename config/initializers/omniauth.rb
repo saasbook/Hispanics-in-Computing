1d79c61809e0e4b55242f1a12929409ba1964c5d
Danelle Nachum <dnachum@berkeley.edu>
2019-11-26 04:49:51 -0800

    fixed issue with omniauth where emails were nil

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"], {
     skip_jwt: true,
     :name => "google",
-    :scope => "profile", 
+    :scope => "email, profile", 
     :prompt => "select_account",
     :image_aspect_ratio => "square",
     :image_size => 50,
