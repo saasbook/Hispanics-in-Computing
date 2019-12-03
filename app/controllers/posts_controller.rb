c4b02e06edaec2801628d8572cf22976ecd5f356
Danelle Nachum <dnachum@berkeley.edu>
2019-12-03 12:20:55 -0800

    fixed issue where visibility_modified? was always true

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
                     :country_of_origin,
                     :map_visibility)
     success = @current_user.update(filtered_params)
-
+    puts visibility_modified?(success, filtered_params[:map_visibility])
     if visibility_modified?(success, filtered_params[:map_visibility])
       flash[:warning] = "Unable to add pin to map:" \
                         " Location must be the name" \
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
 
   private
   def visibility_modified?(success, params_map_visibility)
-    success && params_map_visibility && (not @current_user.map_visibility)
+    success && (params_map_visibility.to_i == 1) && (not @current_user.map_visibility)
   end
 
 end
