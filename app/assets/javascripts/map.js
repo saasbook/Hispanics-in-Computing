d6a839e09907546d5d99cbcbcab8189ec5055f14
shalandy <shalandy@berkeley.edu>
2019-12-05 18:12:16 -0800

    image map bug fix

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -53,7 +53,7 @@ var otherIcon = L.icon({
         imgSrc = thisUser.photo_link
       }
       p.innerHTML = '<p align="center"><img class="map-pfp" src='+ imgSrc
-      + '/></p><h4 style="text-align:center">'+name+'</h4>';
+      + '></p><h4 style="text-align:center">'+name+'</h4>';
       p.innerHTML+= createMapHTML(thisUser.country_of_origin.split(" "));
       p.innerHTML+= '<p align="center" style="font-size:130%;">'+thisUser.profession+' @'+thisUser.organization+'</a></p>';
       p.innerHTML+= '<div align="center"><a target="blank" href='+thisUser.linkedin+' class="edit-profile"><i class="icon-square icon-linkedin icon-2x"></i></a>'
@@ -64,7 +64,7 @@ var otherIcon = L.icon({
   }
 
   function createMapHTML(countries) {
-    // If the countries array only contains the empty string 
+    // If the countries array only contains the empty string
     // then add nothing to the html
     if (countries.length <= 1 || countries[1].length == 0) {
       return "";
@@ -75,4 +75,4 @@ var otherIcon = L.icon({
       mapHTML+= country_name+' '+'<span class=\"flag flag-'+country_name.toLowerCase()+'" style="margin-right: 10px;"></span>';
     }
     return mapHTML + '<br/></p>'
-  } 
+  }
